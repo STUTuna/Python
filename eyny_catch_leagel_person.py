dbe0a399b2920267176e7d5e2435d02c7897ae8a
STUTuna <s15113114@stu.edu.tw>
2018-12-26 17:42:30 +0800

    update

--- eyny_catch_leagel_person.py
+++ eyny_catch_leagel_person.py
@@ -10,7 +10,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 #from time import sleep#拿來做延遲
 
-int_str_rule = 12 #字數限制
+int_str_rule = 10 #字數限制
 url = 'http://www07.eyny.com/thread-10669524-1-3DN3CFFH.html' #欲檢查的網址
 
 html = requests.get(url)
@@ -20,13 +20,13 @@ sp = BeautifulSoup(html.text,'html.parser')
 #browser.maximize_window()#瀏覽器最大化
 
 data = sp.find_all("td", class_="t_f") #取得留言串 包括發文者
-for i in range(len(data)):
+for i in range(len(data)):#跑整頁的留言
     str1 = data[i].text    
     user_content = str1.split('心得：')
     
-    if (len(user_content) > 1) and (len(user_content[1]) < int_str_rule):
+    if (len(user_content) > 1) and (len(user_content[1]) <= int_str_rule):
                
-        user_account = data[i].parent.parent.parent.parent.parent.parent.parent.find(class_="xw1").text
+        user_account = data[i].parent.parent.parent.parent.parent.parent.parent.find(class_="xw1").text #找此回應的作者
         print(user_account)
         print(user_content[1])
 
