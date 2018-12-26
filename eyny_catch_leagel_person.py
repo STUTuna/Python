6e1636bc2b7fd7c96892e82105dc1b847ed282d2
STUTuna <s15113114@stu.edu.tw>
2018-12-26 16:04:22 +0800

    can get reback's auther

--- eyny_catch_leagel_person.py
+++ eyny_catch_leagel_person.py
@@ -10,6 +10,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 #from time import sleep#拿來做延遲
 
+int_str_rule = 12 #字數限制
 url = 'http://www07.eyny.com/thread-10669524-1-3DN3CFFH.html' #欲檢查的網址
 
 html = requests.get(url)
@@ -18,17 +19,15 @@ sp = BeautifulSoup(html.text,'html.parser')
 #browser = webdriver.Chrome()
 #browser.maximize_window()#瀏覽器最大化
 
-data = sp.find_all("td", class_="t_f")
-                 
-#data2 = data.find_all("td", class_="t_f")
-str1 = data[5].text
-print(str1)
-
-result = str1.find('心得：')
-print(result)
-
-print("字串長度為：" , len(str1))
-
-print(str1.split('心得：')[1])
+data = sp.find_all("td", class_="t_f") #取得留言串 包括發文者
+for i in range(len(data)):
+    str1 = data[i].text    
+    user_content = str1.split('心得：')
+    
+    if (len(user_content) > 1) and (len(user_content[1]) < int_str_rule):
+               
+        user_account = data[i].parent.parent.parent.parent.parent.parent.parent.find(class_="xw1").text
+        print(user_account)
+        print(user_content[1])
 
 print("Done！")
